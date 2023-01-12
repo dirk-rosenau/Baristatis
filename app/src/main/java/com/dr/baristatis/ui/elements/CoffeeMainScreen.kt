7b0e9193b92c42a867393e1a5c62e976ebed2dd1
Dirk Rosenau <dirk.rosenau@hotmail.de>
2023-01-12 14:33:25 +0100

    Use screen state

--- app/src/main/java/com/dr/baristatis/ui/elements/CoffeeMainScreen.kt
+++ app/src/main/java/com/dr/baristatis/ui/elements/CoffeeMainScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -15,40 +16,81 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.dr.baristatis.R
 import com.dr.baristatis.commons.OnMyCoffeeItemClicked
+import com.dr.baristatis.model.MyCoffeeData
 import com.dr.baristatis.ui.vm.MainViewModel
 
 @Composable
 fun CoffeeMainScreen(viewModel: MainViewModel, onMyCoffeeItemClicked: OnMyCoffeeItemClicked) {
-    val coffees = viewModel.coffees.collectAsState()
+    val viewState = viewModel.viewState.collectAsState()
     Box(Modifier.fillMaxWidth()) {
-        if (coffees.value.isEmpty()) {
-            EmptyMainScreen()
-        } else {
-            LazyColumn {
-                items(coffees.value) { coffee ->
-                    CoffeeCard(myCoffeeData = coffee, onMyCoffeeItemClicked = onMyCoffeeItemClicked)
-                }
+        when (viewState.value) {
+            is MainViewModel.Success -> {
+                CoffeeDataView(
+                    (viewState.value as MainViewModel.Success).data,
+                    onMyCoffeeItemClicked
+                )
+            }
+            is MainViewModel.Error -> ErrorScreen()
+            is MainViewModel.Loading -> LoadingScreen()
+        }
+    }
+}
+
+// TODO CompositionLocal benutzen?
+@Composable
+fun CoffeeDataView(coffees: List<MyCoffeeData>, onMyCoffeeItemClicked: OnMyCoffeeItemClicked) {
+    if (coffees.isEmpty()) {
+        EmptyListScreen()
+    } else {
+        LazyColumn {
+            items(coffees) { coffee ->
+                CoffeeCard(myCoffeeData = coffee, onMyCoffeeItemClicked = onMyCoffeeItemClicked)
             }
         }
     }
 }
 
 @Composable
-fun EmptyMainScreen() {
+fun LoadingScreen() {
+    MainScreenPlaceholder(imageResource = R.drawable.empty_bag, showLoading = true)
+}
+
+
+@Composable
+fun ErrorScreen() {
+    MainScreenPlaceholder(imageResource = R.drawable.error_bag, textResource = R.string.error)
+}
+
+@Composable
+fun EmptyListScreen() {
+    MainScreenPlaceholder(imageResource = R.drawable.empty_bag, textResource = R.string.addCoffee)
+}
+
+@Composable
+fun MainScreenPlaceholder(
+    imageResource: Int,
+    textResource: Int = -1,
+    showLoading: Boolean = false
+) {
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(id = R.drawable.empty_bag), // TODO use image from data
+            painter = painterResource(id = imageResource),
             contentDescription = "",
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .padding(10.dp)
                 .size(200.dp)
-
         )
-        Text(text = stringResource(id = R.string.addCoffee))
+        if (textResource != -1) {
+            Text(text = stringResource(id = textResource))
+        }
+
+        if (showLoading) {
+            CircularProgressIndicator()
+        }
     }
 }
--- app/src/main/java/com/dr/baristatis/ui/vm/MainViewModel.kt
+++ app/src/main/java/com/dr/baristatis/ui/vm/MainViewModel.kt
@@ -1,6 +1,6 @@
 package com.dr.baristatis.ui.vm
 
-import androidx.compose.runtime.mutableStateListOf
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dr.baristatis.model.MyCoffeeData
@@ -10,21 +10,31 @@ import kotlinx.coroutines.launch
 
 class MainViewModel(private val coffeeRepository: CoffeeRepository) : ViewModel() {
 
-    private val _state = MutableStateFlow<MainViewState>(Idle)
-    val state: StateFlow<MainViewState> = _state
+    private val _state = MutableStateFlow<MainViewState>(Loading)
+    val viewState: StateFlow<MainViewState> = _state
 
     sealed class MainViewState
-    object Idle : MainViewState()
     object Loading : MainViewState()
-    data class Finished(val data: List<MyCoffeeData>) : MainViewState()
-    data class Error(val message: String) : MainViewState()
+    data class Success(val data: List<MyCoffeeData>) : MainViewState()
+    data class Error(val throwable: Throwable) : MainViewState()
 
-    val coffees = coffeeRepository.getCoffees().stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
+    init {
+        coffeeRepository.getCoffees()
+            .onStart { _state.emit(Loading) }
+            .onEach {
+                _state.emit(Success(it))
+            }
+            .catch { throwable ->
+                _state.emit(Error(throwable))
+                throwable.message?.let { Log.d("MainViewModel", it) }
+            }
+            .launchIn(viewModelScope)
+    }
 
     //TODO method in dao
-    //fun getItem(itemId: Int) = coffeeDataList.find { it.id == itemId }
-
-    fun getItem(itemId: Int) = coffees.value.find { it.id == itemId }
+    fun getItem(itemId: Int) = (_state.value as? Success)?.let {
+        it.data.find { it.id == itemId }
+    }
 
     fun saveCoffee(coffeeData: MyCoffeeData) {
         viewModelScope.launch {
@@ -32,7 +42,7 @@ class MainViewModel(private val coffeeRepository: CoffeeRepository) : ViewModel(
         }
     }
 
-    fun deleteCoffeeData(id: Int){
+    fun deleteCoffeeData(id: Int) {
         viewModelScope.launch {
             coffeeRepository.deleteCoffee(id)
         }
